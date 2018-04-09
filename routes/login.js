1f3d7f804a77d3606debd00a6b227356ce0c8296
Jesus Laucho <jlaucho@gmail.com>
2018-04-09 16:34:49 -0400

    login por google casi listo

--- routes/login.js
+++ routes/login.js
@@ -3,11 +3,64 @@ var express = require('express');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 var SEED = require('../config/config').SEED;
+const {OAuth2Client} = require('google-auth-library');
 
 // Inicializar variables
 var app = express();
 var Usuario = require('./../models/usuario');
 
+const client_id = require('./../config/config').client_id;
+const secret_id = require('./../config/config').secret_id;
+
+
+/**
+ * AUTENTICACION POR GOOGLE
+ */
+
+
+app.post('/google', (request, response, next)=>{
+    
+    token = request.body.token || 'as';
+    
+    const client = new OAuth2Client(client_id);
+
+    async function verify() {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: client_id,  // Specify the CLIENT_ID of the app that accesses the backend
+            // Or, if multiple clients access the backend:
+            //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+        });
+        const payload  = ticket.getPayload();
+        const userid = payload['sub'];
+        // If request specified a G Suite domain:
+        //const domain = payload['hd'];
+    /**=================================================
+     * 
+     * =================================================
+     */
+    return response.status( 200 ).json({
+        'ok':true,
+        'mensaje': 'Peticion realizada correctamente',
+        'payload': payload
+    });
+       
+    }
+
+    verify().catch((e)=>{
+        return response.status( 400 ).json({
+            'ok':false,
+            'mensaje': 'Error al validat el token',
+            'errors': e
+        });
+    });
+    
+    
+});
+
+/**
+ * ATENTICACION CON EMAIL Y PASSWORD
+ */
 app.post( '/', ( request, response, next ) =>{
     body = request.body;
 
@@ -50,5 +103,6 @@ app.post( '/', ( request, response, next ) =>{
     });
 
 });
+
 
 module.exports = app;
