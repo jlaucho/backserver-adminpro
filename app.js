750aae5727b7faa55758c1a0728b25cc5d58ddf1
Jesus Laucho <jlaucho@gmail.com>
2018-04-17 15:50:27 -0400

    ajustando los CORS

--- app.js
+++ app.js
@@ -14,6 +14,14 @@ mongoose.connection.openUri('mongodb://localhost:27017/hospitalDB', (error, resp
 // Inicializar variables
 var app = express();
 
+// CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "*");
+    next();
+});
+
 // Body parse
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
